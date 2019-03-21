096582f671398c911de82e2d2efdbb9c61ede34d
Brad Schrag <brad.schrag@gmail.com>
2019-03-21 15:13:44 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+let main = document.getElementById('main');
+ 
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
+let element = document.createElement('div')
+
+element.innerHTML = 'Hello, DOM!'
+element.style.backgroundColor = '#f9f9f9';
+
+document.body.appendChild(element);
