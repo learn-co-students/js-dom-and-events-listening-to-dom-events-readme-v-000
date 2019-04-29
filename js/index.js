c58c8a1624a722542a079251879c66ae34fdb390
FionaDL <github email address>
2019-04-29 15:22:59 +0200

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const divs = document.getElementByTagName('div');
+
+divs.addEventListener('click', function(event) {
+  alert('help me!!');
+});
