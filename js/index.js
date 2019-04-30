9e9cdc155b78a4a579059ad644e3f5bc7dcc3d74
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-04-30 05:11:30 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const divs = document.querySelectorAll('div');
+
+divs.addEventListener('click', function(e) {
+  alert("Don't touch my divs!");
+})
