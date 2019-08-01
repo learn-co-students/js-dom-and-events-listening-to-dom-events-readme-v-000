f1f5dd124aad20046d64a8b3d6cb8d6715402f6c
Heshie Brody <4hbrody@gmail.com>
2019-08-01 00:00:11 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,42 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const page = document;
+const main = document.getElementById('main');
+const input = document.querySelector('input');
+
+let divs = document.querySelectorAll('div');
+let keyPressed = "";
+
+page.addEventListener ('keydown', function (e) {
+    if (e.key === 13) {
+      console.log('enter key ignored');
+      return e.preventDefault();
+    } else {
+      keyPressed += e.key;
+      console.log(keyPressed);
+    }
+  }
+);
+
+main.addEventListener ('click', function (event) {
+  alert('I was clicked!');
+});
+
+input.addEventListener ('keydown', function (e) {
+  if (e.key === 71) {
+    console.log('default prevented');
+    return e.preventDefault();
+  } else {
+    console.log('Not a g');
+  }
+});
+
+function bubble (e) {
+  e.stopPropagation();
+
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
